33337e0f630fb4e392d07e7e2ef75d2f8abb7492
LucasCrtt <carottalucas@gmail.com>
2026-02-19 18:22:58 -0300

    feat: add JWT authentication guard to UserController

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserFilterDto } from './dto/user-filter.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guards';
 
+@UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UserController {
      constructor(private readonly userService: UserService) {}
